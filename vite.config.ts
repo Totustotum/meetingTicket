c40e32d538b253ebbc2adf24526f8d1017f26583
David Oh <doh3@des-doh3-mbp.lan>
2026-02-17 22:42:06 -0500

    Fix base path for meetingTicket repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/scheduling-app/', // Change this to your GitHub repo name, or '/' if repo is username.github.io
+  base: '/meetingTicket/', // GitHub Pages: https://totustotum.github.io/meetingTicket/
 })
